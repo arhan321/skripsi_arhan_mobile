fc61d3ffc0acddfdbc8daff385ce43443b30e392
arhan321 <arhanmcz@gmail.com>
2026-07-23 05:12:52 +0700

    freshg

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,7 +3,7 @@ class AppConfig {
 
   /// Ganti sesuai domain Laravel kamu.
   /// Mobile sebaiknya call Laravel API, bukan langsung ke FastAPI ML.
-  static const String apiBaseUrl = 'https://prediksi.djncloud.my.id/api';
+  static const String apiBaseUrl = 'https://tourhub.my.id/api';
 
   static const String appName = 'TourHub Bali';
 }
